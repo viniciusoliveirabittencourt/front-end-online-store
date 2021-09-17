1e4f0db5e2b73cb9566cfa0492486ad1f5e53033
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-09-17 16:51:55 -0300

    Parte 1 ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const obj = response.json();
+  return obj;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
