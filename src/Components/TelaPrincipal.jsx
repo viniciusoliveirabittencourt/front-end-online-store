4eff93ce84a9db2aabb1a6db78f0b0b1f1607a45
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-09-20 17:35:59 -0300

    5° Req

--- src/Components/TelaPrincipal.jsx
+++ src/Components/TelaPrincipal.jsx
@@ -1,39 +1,89 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getProductsFromCategoryAndQuery } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import CardCategories from './CardCategories';
+import ProductCard from './ProductCard';
 
 class TelaPrincipal extends React.Component {
   constructor() {
     super();
-    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       products: [],
       searchFor: '',
-    }
+      categories: [],
+    };
   }
 
-  async handleChange({ target }) {
+  componentDidMount() {
+    this.getAllCategories();
+  }
+
+  handleChange = ({ target }) => {
     const { name, value } = target;
-    const categoryId = "MLB1051";
-    const search = await getProductsFromCategoryAndQuery(categoryId, value);
-    console.log(search.results);
-    this.setState({ [name]: value, products: search.results });
+    this.setState({ [name]: value });
+  }
+
+  callApi = async () => {
+    const { searchFor } = this.state;
+    const search = await getProductsFromCategoryAndQuery('', searchFor);
+    this.setState({ products: search.results });
+  }
+
+  getAllCategories = async () => {
+    const data = await getCategories();
+    this.setState({ categories: data });
   }
 
   render() {
-    const { searchFor, products } = this.state;
+    const { searchFor, categories, products } = this.state;
     return (
       <div>
-        <input type="text" name="searchFor" onChange={ this.handleChange } value={ searchFor } />
-        <h2 data-testid="home-initial-message">
+        <label htmlFor="searchFor" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </h2>
+          <input
+            data-testid="query-input"
+            type="text"
+            name="searchFor"
+            onChange={ this.handleChange }
+            value={ searchFor }
+          />
+        </label>
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.callApi }
+        >
+          Search
+        </button>
 
         <Link data-testid="shopping-cart-button" to="/carrinho">
-          <span>&#x1F6D2</span> carrinho de compras
+          <span
+            role="img"
+            aria-label="carrinho compra icone"
+          >
+            &#x1F6D2;carrinho de compras
+          </span>
         </Link>
-        {products.map(({ title, price }) => <div><h1>{ title }</h1><h2>{ price }</h2></div>)}
-
+        <section>
+          <ul>
+            {categories
+              .map((element) => (<CardCategories
+                key={ element.id }
+                category={ element.name }
+              />))}
+          </ul>
+        </section>
+        <section>
+          {products
+            .map(({ title, price, id, thumbnail }) => (
+              <ProductCard
+                key={ id }
+                img={ thumbnail }
+                title={ title }
+                price={ price }
+              />))}
+        </section>
       </div>
     );
   }
