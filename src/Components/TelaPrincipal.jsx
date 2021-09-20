6fb8c6e92e63de0a66eb5e6112d10e7391702b6e
ronaldosenajr <ronaldosenajr@gmail.com>
2021-09-20 14:59:26 -0400

    Requisito 4 concluido

--- src/Components/TelaPrincipal.jsx
+++ src/Components/TelaPrincipal.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
+import CardCategories from './CardCategories';
 
 class TelaPrincipal extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [] };
+  }
+
+  componentDidMount() {
+    this.getAllCategories();
+  }
+
+  getAllCategories = async () => {
+    const data = await getCategories();
+    this.setState({ categories: data });
+    console.log(data);
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" value="aaa" />
@@ -13,6 +32,13 @@ class TelaPrincipal extends React.Component {
         <Link data-testid="shopping-cart-button" to="/carrinho">
           &#x1F6D2; carrinho de compras
         </Link>
+        <ul>
+          {categories
+            .map((element) => (<CardCategories
+              key={ element.id }
+              category={ element.name }
+            />))}
+        </ul>
 
       </div>
     );
