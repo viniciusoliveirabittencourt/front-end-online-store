c04d99e542351844b1a88b9bed1cd9d0c6302011
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-11-22 11:47:57 -0300

    commit para o repositorio pessoal

--- src/Components/TelaCarrinhoCompras.jsx
+++ src/Components/TelaCarrinhoCompras.jsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CardCarrinhoCompras from './CardCarrinhoCompras';
+import CarrinhoComponent from './CardCarrinhoCompras';
+import { Link } from 'react-router-dom';
 
 class TelaCarrinhoCompras extends React.Component {
   constructor() {
     super();
     this.state = { cartItems: [] };
+    this.addCarrinhoComponent = this.addCarrinhoComponent.bind(this);
+    this.valorCompra = this.valorCompra.bind(this);
   }
 
   componentDidMount() {
     this.addCarrinhoComponent();
   }
 
-  addCarrinhoComponent = () => {
+  valorCompra(target) {
+    const { name } = target;
+    const value = name === 'add' ? this.cartComponentsAdd() : this.cartComponentsSubb();
+    console.log(value);
+  }
+
+  addCarrinhoComponent() {
     const { location } = this.props;
     const { carrArr } = location;
-    console.log('location: ', location);
-    console.log('carrArr: ', carrArr);
-    const items = carrArr.map(({ title, price, img }, index) => (<CardCarrinhoCompras
+    const items = carrArr.map(({ title, price, img }, index) => (<CarrinhoComponent
       title={ title }
       price={ price }
       img={ img }
       key={ index }
+      onClick={ this.valorCompra }
     />));
     this.setState({ cartItems: items });
   }
@@ -32,13 +40,20 @@ class TelaCarrinhoCompras extends React.Component {
     const { cartItems } = this.state;
     return (
       <div>
+        <Link to="/">
+            <span>
+              return
+            </span>
+          </Link>
         <h1
           data-testid="shopping-cart-product-quantity"
         >
           { carrArr ? carrArr.length : 0 }
         </h1>
-        { cartItems.length !== 0 ? cartItems
+        { cartItems
+          .length !== 0 ? cartItems
           : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p> }
+        <h1>Valor Total da Compra: R$ {}</h1>
       </div>
     );
   }
